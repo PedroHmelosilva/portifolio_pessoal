0ff29e7d4a37badfee9f754d230118d01acd2348
Pedro Henrique Melo E Silva <119549760+PedroHmelosilva@users.noreply.github.com>
2022-12-04 19:16:29 -0300

    Site01
    Inserindo o modo escuro

--- pedro/scriptpedro.js
+++ pedro/scriptpedro.js
@@ -56,3 +56,31 @@ sr.reveal('.meus-conhecimentos-align', {
     rotate: {x: 0, y:0, z:0},
     duration: 4000
 });
+
+/*Modo escuro*/ 
+
+const changeThemeBtn = document.querySelector("#change-theme")
+
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark")
+
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkMode();
+
+    localStorage.removeItem("dark")
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
