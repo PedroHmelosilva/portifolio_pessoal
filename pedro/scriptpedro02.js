e680c4f57f4aff2c4fc256de3518722493a76b9e
Pedro Henrique Melo E Silva <119549760+PedroHmelosilva@users.noreply.github.com>
2022-12-04 21:31:36 -0300

    Site02
    Implementando o modo escuro no site de referências.

--- pedro/scriptpedro02.js
+++ pedro/scriptpedro02.js
@@ -14,3 +14,31 @@ sr.reveal('#texto-html5, .tabela',{
     rotate: {x: 0, y:0, z:0},
     duration: 2000
 });
+
+/*Modo escuro*/ 
+
+const changeThemeBtn = document.querySelector("#change-theme")
+
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark")
+
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkMode();
+
+    localStorage.removeItem("dark")
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
